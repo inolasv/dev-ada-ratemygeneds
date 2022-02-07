b7429a25d30186cfe4ef43792e152cec7ba4531a
Meg Li <mli3023@gmail.com>
2022-02-07 12:41:55 -0600

    start

---
+++ rate-geneds/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Search from './Search';
+
+ReactDOM.render(<Search />, document.getElementById('root'));
+
+// var element = React.createElement('h1', { className: 'appname' }, 'Rate My Geneds @ UIUC');// ReactDOM.render(element, document.getElementById('root'));
+
+
