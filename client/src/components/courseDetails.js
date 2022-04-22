e901a991931486368215834e8e52c1d8bbaa8c8c
Saloni Vaishnav <inolasv@gmail.com>
2022-04-22 17:48:18 -0500

    styled course page a little

--- client/src/components/courseDetails.js
+++ client/src/components/courseDetails.js
@@ -2,6 +2,25 @@ import React, { Component, useState , useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import CourseList from "./courseList";
 import "./Home";
+import star from '../star.png';
+import "../stylesheets/course.css"
+
+
+
+const determineClass = (type) => {
+  if (type === "") {
+    return "none"
+  }
+  return "box"
+}
+
+const makeStars = () => {
+  const stars = [];
+  for(var i = 0; i < Math.random() * 5; i++) {
+    stars.push(<img src={star} alt="Star" className="star"/>);
+  }
+  return stars;
+}
 
 const SingleGenEd = () => {
     const [course, setCourse] = useState([]);
@@ -40,13 +59,21 @@ const SingleGenEd = () => {
                 <h3> Course Description:</h3>
               </div>
               <div className="ratings">
-                <h3> Average Rating</h3> 
+                Average Rating: 
+                {makeStars()}
                 <image src="https://icon2.cleanpng.com/20171220/ofw/star-png-image-5a3a7e9651e390.02167008151378293433541614.jpg" alt="display image"/> 
-                <h3> Number of Ratings: </h3>
+                <div/>
+                Number of Ratings:
               </div>
               <div className="categories">
-                <h3> Categories: {course['ACP']} {course['CS']} {course['HUM']} {course['NAT']} {course['QR']} {course['SBS']}</h3> 
-              </div>
+                Categories: 
+                  <p className={determineClass(course['ACP'])}>{ course['ACP'] }</p> 
+                  <p className={determineClass(course['CS'])}>{ course['CS'] }</p> 
+                  <p className={determineClass(course['HUM'])}>{ course['HUM'] }</p> 
+                  <p className={determineClass(course['NAT'])}>{ course['NAT'] }</p> 
+                  <p className={determineClass(course['QR'])}>{course['QR']}</p> 
+                  <p className={determineClass(course['SBS'])}>{course['SBS']}</p>
+              </div>              
             </div>
             );
 }
